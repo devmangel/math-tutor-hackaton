259f1316a9805910205a480ea5735c42137c4d4b
MarlonArcila <155872432+MarlonArcila@users.noreply.github.com>
2025-05-11 04:59:43 -0500

    App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,7 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import { v4 as uuidv4 } from "uuid";
 import Image from "next/image";
-import { motion, px } from "framer-motion";
-import { BookOpen, User, Lightbulb } from 'lucide-react';
 
 // UI components
 import TutorMainView from "./tutor-ai/TutorMainView";
